7df68c481cd8477ec2b981bea3da3f9c110339d3
zkl <1581981257@qq.com>
2024-11-04 13:46:50 +0800

    初始提交，完成巡检机器人

---
+++ install/autopatrol_interfaces/include/autopatrol_interfaces/autopatrol_interfaces/srv/speach_text.hpp
@@ -0,0 +1,11 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef AUTOPATROL_INTERFACES__SRV__SPEACH_TEXT_HPP_
+#define AUTOPATROL_INTERFACES__SRV__SPEACH_TEXT_HPP_
+
+#include "autopatrol_interfaces/srv/detail/speach_text__struct.hpp"
+#include "autopatrol_interfaces/srv/detail/speach_text__builder.hpp"
+#include "autopatrol_interfaces/srv/detail/speach_text__traits.hpp"
+
+#endif  // AUTOPATROL_INTERFACES__SRV__SPEACH_TEXT_HPP_
